a2bc2524ab3ffbb4c754334ad8c03cd6867bc30f
Redy <redy.ru@gmail.com>
2014-11-06 03:17:17 +0800

    update editor

--- editor/scripts/editor.js
+++ editor/scripts/editor.js
@@ -11,6 +11,7 @@ supMockEditor.constant('Config', {
 	site_config: CONFIG.site_config,
 	theme_config: CONFIG.theme_config,
 	editor_params:CONFIG.editor_params,
+	mock_query:CONFIG.mock_query,
 });
 
 /* Route */
@@ -49,7 +50,8 @@ supMockEditor.directive('editorCanvas', function ($http, $compile,  Config) {
             'file': '=',
             'config':'=',
             'app': '@',
-            'type': '@'
+            'type': '@',
+			'query':'=',
         },
         compile: function () {
             return function (scope, element) {
@@ -61,21 +63,37 @@ supMockEditor.directive('editorCanvas', function ($http, $compile,  Config) {
 				
 				$http.get(tpl_url).success(function(data, status, headers, config) {
                     element.html(data);
-                    var A = element[0].querySelectorAll('a[fake]');
-					
-                    for (var i = 0; i < A.length; i++) {
-                        A[i].addEventListener('click', function (e) {
-                            e.preventDefault();
-                            return false;
-                        });
-                    }
                     $compile(element.contents())(scope);
 				});
             };
         }
     };
 });
-
+supEditor.directive('stopEvent', function () {
+      'use strict';
+      return {
+          restrict: 'A',
+          link: function (scope, element, attr) {
+              element.bind('click', function (e) {
+                  e.stopPropagation();
+              });
+          }
+      };
+   });
+   supEditor.directive('fakeLink', function () {
+         'use strict';
+         return {
+             restrict: 'A',
+             link: function (scope, element, attr) {
+                 element.bind('click', function (e) {
+                     e.stopPropagation();
+                     e.preventDefault();
+                     return false;
+                 });
+              
+             }
+         };
+      }); 
 supMockEditor.directive('supEditorMeta', function () {
       return {
           restrict: 'A', // only activate on element attribute
@@ -116,3 +134,19 @@ supEditor.directive('supEditorWidget', function ($rootScope) {
           }
       };
   });
+  supEditor.directive('supEditorQuery', function (EditorDataQuery,Config) {
+        'use strict';
+        return {
+            restrict: 'A', // only activate on element attribute
+            controller: function($scope,$element,$attrs){
+                    var data_query=$scope.$eval($attrs.supEditorQuery);
+					var mock_name=$attrs.mock
+					console.log('------------------------------------');
+					console.log('Note: Mock attributes is only for local testing, just write down the mock data intro config.js');
+					console.log('Query Object is:');
+					console.log(data_query)
+					console.log('------------------------------------');
+					$scope.query[$attrs.results]=Config.mock_query[mock_name];
+            }
+        };
+    });
