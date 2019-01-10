f638e3c93f44a2f300d6be907b940b6f7b94fbcc
redy <redy.ru@gmail.com>
2019-01-11 02:56:18 +0800

    ver

--- pyco.py
+++ pyco.py
@@ -15,7 +15,7 @@ from analyzer import SimpleAnalyzer
 from blueprints import register_blueprints
 
 
-__version_info__ = ('2', '18', '6')
+__version_info__ = ('2', '19', '0')
 __version__ = '.'.join(__version_info__)
 
 
