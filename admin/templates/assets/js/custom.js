e72dbe97cce9eb4dc0c5f91d2e81a8fd0587115b
redy <redy.ru@gmail.com>
2019-12-12 04:36:39 +0800

    xxx

--- admin/templates/assets/js/custom.js
+++ admin/templates/assets/js/custom.js
@@ -82,7 +82,6 @@ $(document).ready(function() {
 
   /* Media Input Field */
   $('.media-input-field').each(function(){
-    console.log('fuck111');
     var media_input = $(this).find('.media-input');
     var preview = $(this).find('.media-preview');
     var preview_link = preview.find('a');
