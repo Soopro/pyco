66a9810a229d1b8912d015d11d68e1422675d4bb
redy <redy.ru@gmail.com>
2020-06-20 22:53:21 +0800

    fix prevent input

--- admin/templates/assets/js/custom.js
+++ admin/templates/assets/js/custom.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
   });
 
   /* prevent ENTER key */
-  $('.prevent-enter-key').on('keyup keypress', function(e) {
+  $('.prevent-enter-key input').on('keyup keypress', function(e) {
     var keyCode = e.keyCode || e.which;
     if (keyCode === 13) {
       e.preventDefault();
--- info.py
+++ info.py
@@ -1,4 +1,4 @@
 # coding=utf-8
 
-__version_info__ = ('3', '7', '6')
+__version_info__ = ('3', '7', '7')
 __version__ = '.'.join(__version_info__)
